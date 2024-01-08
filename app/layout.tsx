e09490ed66ff9200fe95db7e859135009567a4cf
Ondrej Suchy <suchyond@gmail.com>
2024-01-08 20:03:55 +0100

    Initial ant.d and antv/g2 experiments

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { Header } from 'antd/es/layout/layout';
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
   <html lang="en">
     <body>
+      <Header style={{ color: "white" }}>
+        <h1>One hour demo for MSD</h1>
+      </Header>
       <AntdRegistry>{children}</AntdRegistry>
     </body>
   </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import { Button } from 'antd';
+"use client"
+import React, { useEffect } from 'react';
+import { Button, Layout, Card, Row, Col } from 'antd/es';
+import { renderChart } from './chart';
 
-const Home = () => (
+const Home = () => {
+  useEffect(() => {
+    renderChart();
+  }, []);
+
+  return (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Row gutter={16}>
+      <Col span={8}>
+        <Card title="Card title 1">
+          <div>
+            <Button type="primary">Button</Button>
+            <div id={'chart01'}/>
+          </div>
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title 2" bordered={false}>
+          Card content
+          <div id={'chart02'}/>
+        </Card>
+      </Col>
+    </Row>
   </div>
-);
+)};
 
 export default Home;
